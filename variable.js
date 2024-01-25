fcec0fa2144322943f2cd26ad24e9c1e3d30d984
saiful797 <saifulislamsaif797@gmail.com>
2024-01-25 23:43:22 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,13 @@
+/**
+ * VARIABLE==>VARY+ABLE
+ * 5 THINGS TO VARIABLE DECLARE
+ */
+
+ var weight=48;
+ var price=48;
+ var year=1995;
+ var money=2013;
+
+ console.log(money);
+ console.log(price);
+ console.log(year);
